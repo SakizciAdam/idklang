bd012a6f0157ea0310e569ab8e0fbe8850936b37
SakizciAdam <durulozcan@hotmail.com>
2022-10-14 23:22:46 +0300

    sleep

--- idklang/time.hpp
+++ idklang/time.hpp
@@ -1,8 +1,18 @@
 #include <chrono>
+#include <thread>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 using namespace std::chrono;
 
 uint64_t millis() {
   
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+void sleep(int x){
+  std::this_thread::sleep_for(milliseconds(x));
 }
