6545eb19d33413c61eb4b5a6ebc39c726560d5da
SakizciAdam <durulozcan@hotmail.com>
2022-10-15 16:09:29 +0300

    faster reader

---
+++ include/reader.h
@@ -0,0 +1,14 @@
+
+extern "C"
+{
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+}
+#ifndef READER_H_   /* Include guard */
+
+#define READER_H_
+
+char* read(char* fileName);  /* An example function declaration */
+
+#endif // READER_H_
---
+++ reader.c
@@ -0,0 +1,26 @@
+#include "./include/reader.h"
+
+
+
+char *read(char *fileName)
+{
+    FILE *file = fopen(fileName, "r");
+    char *code;
+    size_t n = 0;
+    int c;
+
+    if (file == NULL)
+        return NULL; //could not open file
+
+    code = (char*)malloc(1000);
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        code[n++] = (char) c;
+    }
+
+    // don't forget to terminate with the null character
+    code[n] = '\0';        
+
+    return code;
+}
